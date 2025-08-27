077ff1a2c53f60f271d86440ae4418f98fbc65ed
Saul <125879463+saul-stack@users.noreply.github.com>
2025-08-27 18:08:19 +0100

    chore: move handleError function

--- utils/js/index.js
+++ utils/js/index.js
@@ -1,7 +1,7 @@
 import addNumbers from "./addNumbers.js";
 import fetchJson from "./fetchJson.js";
 import getSmallest from "./getSmallest.js";
-import handleError from "./handleError.js";
+import handleError from "../global/handleError.js";
 import printString from "./printString.js";
 
 export { addNumbers, fetchJson, getSmallest, handleError, printString };
