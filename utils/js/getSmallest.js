09e31d33d7d3792c202e66056fea1bc85c7a97f7
Saul <125879463+saul-stack@users.noreply.github.com>
2025-08-27 18:08:40 +0100

    dev: use handleError in getSmallest function

--- utils/js/getSmallest.js
+++ utils/js/getSmallest.js
@@ -1,16 +1,18 @@
+import { handleError } from "./index.js";
+
 const getSmallest = (numbers) => {
   if (!Array.isArray(numbers)) {
-    throw new Error("Input must be an array");
+    handleError("Input must be an array");
   }
 
   if (numbers.length < 1) {
-    throw new Error("Input array must have length > 0");
+    handleError("Input array must have length > 0");
   }
 
   let currentSmallest = numbers[0];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i].isNaN) {
-      throw new Error("Input array must contain only numbers");
+      handleError("Input array must contain only numbers");
     }
     currentSmallest > numbers[i + 1]
       ? (currentSmallest = numbers[i + 1])
