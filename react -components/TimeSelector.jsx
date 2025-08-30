e7c9f64713fd27bf17d5d03554daae37819bd0b6
Saul <125879463+saul-stack@users.noreply.github.com>
2025-08-30 14:08:18 +0100

    feat: create TimeSelector component

---
+++ react -components/TimeSelector.jsx
@@ -0,0 +1,48 @@
+//  TimeSelector component.
+//  Users can select an hour and minute from a dropdown menu.
+//  When the selected time is updated, handleTimeChange function is executed
+
+import { useEffect, useState } from "react";
+import { DropDown } from "./DropDown";
+
+export const TimeSelector = () => {
+  const handleTimeChange = () => {
+    //Do this when the time is updated
+  };
+
+  let hoursValues = Array.from({ length: 24 }, (_, i) => i);
+  let hoursStrings = hoursValues.map((i) => String(i).padStart(2, "0"));
+
+  let hours = hoursValues.map((_, i) => {
+    return { value: hoursValues[i], string: hoursStrings[i] };
+  });
+
+  let minutesValues = Array.from({ length: 12 }, (_, i) => i * 5);
+  let minutesStrings = minutesValues.map((i) => String(i).padStart(2, "0"));
+
+  let minutes = minutesValues.map((_, i) => {
+    return { value: minutesValues[i], string: minutesStrings[i] };
+  });
+
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const hourString = String(hour).padStart(2, "0");
+    const minuteString = String(minute).padStart(2, "0");
+    const timeString = `${hourString}${minuteString}`;
+    setTime(timeString);
+  }, [hour, minute]);
+
+  useEffect(() => {
+    handleTimeChange();
+  }, [time]);
+
+  return (
+    <div className="time-dropdown">
+      <DropDown onChange={setHour} options={hours} />
+      <DropDown onChange={setMinute} options={minutes} />
+    </div>
+  );
+};
